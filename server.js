5b4ded7e86c7fcaa4a25068cfc2e3394bd8582b3
Ari Olvera <badillo.ari@gmail.com>
2021-04-02 11:26:54 -0600

    feat (Back-End): Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * Server.js - Entry Point for Back-end
+ * Common JS Syntax for importing with NODE
+ * React uses ES2015 or ES6
+ */
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to contact keeper API' })
+})
+
+//Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server started on ${PORT} `))
