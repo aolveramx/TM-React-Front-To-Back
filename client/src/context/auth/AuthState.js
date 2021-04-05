100c80984ad84d7de14d77ea6f45593977e846dc
Ari Olvera <badillo.ari@gmail.com>
2021-04-05 18:04:04 -0500

    feat: User Registration

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react'
+import axios from 'axios'
 import AuthContext from './authContext'
 import authReducer from './authReducer'
 import {
@@ -26,13 +27,34 @@ const AuthState = (props) => {
   //Load User
 
   //Register User
+  const register = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const response = await axios.post('/api/users', formData, config)
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: response.data
+      })
+    } catch (error) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: error.response.data.message
+      })
+    }
+  }
 
   //Login User
   
   //Logout
 
   //Clear Errors
-
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS })
 
   return (
     <AuthContext.Provider
@@ -42,6 +64,9 @@ const AuthState = (props) => {
         loading: state.loading,
         user: state.user,
         error: state.error,
+        register,
+        clearErrors
+
       }}
     >
       {props.children}
